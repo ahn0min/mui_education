447c00a78712a46a51a3972209630a9fb9167dce
ahn0min <ahn0min95@gmail.com>
2022-01-20 11:18:34 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function App() {
+  return <></>;
+}
+
+export default App;
